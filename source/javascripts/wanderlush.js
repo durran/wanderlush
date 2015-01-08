a169b856222d9a5d94b830c0c10a6a5b492d9770
Durran Jordan <durran@gmail.com>
2015-01-08 22:17:35 +0100

    Adding in information cell

--- source/javascripts/wanderlush.js
+++ source/javascripts/wanderlush.js
@@ -12,3 +12,29 @@
     });
   };
 }(jQuery));
+
+/*
+ * Behaviour that the site actual implements here.
+ */
+$(document).ready(function() {
+
+  /*
+   * Lazy load all images in the large and small boxes.
+   */
+  $(".large-box, .small-box").lazyLoad();
+
+  /*
+   * All bar name boxes show the bar information and review box overlay
+   * when clicked.
+   */
+  $(".bar-container").click(function() {
+    var content = $(this).find(".content-box");
+    var display = $(content).css("visibility");
+    if (display == "hidden") {
+      $(content).css("visibility", "visible");
+    }
+    else {
+      $(content).css("visibility", "hidden");
+    }
+  });
+});
